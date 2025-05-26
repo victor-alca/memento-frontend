f4dcc6086093ddb51244372c73ba0c61cec8bae9
victor-alca <54454708+victor-alca@users.noreply.github.com>
2025-05-26 20:00:34 -0300

    feat: integração com o supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'pages/resultado_teste.dart';
 import 'pages/teste_memoria.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://ljymwvozzshiiwlgdgxc.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqeW13dm96enNoaWl3bGdkZ3hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNzc2NzgsImV4cCI6MjA2Mzc1MzY3OH0.1bH3WSb-HDBHfEEET4l1qYzHL0DIlzHTAiZpUFS4Rlw',
+  );
   runApp(const MyApp());
-}
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
