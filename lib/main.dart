2ccf177642a8d9035c6044ebf7c22b743cf11785
Lyandra Lisboa <lyandralisboa08@gmail.com>
2025-08-22 08:12:44 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
+
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:test_app/app/app.dart';
 import 'package:test_app/app/config/env.dart';
-import 'pages/resultado_teste.dart';
-import 'pages/teste_memoria.dart';
-import 'pages/teste_tmt_a.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,20 +10,8 @@ Future<void> main() async {
   await Supabase.initialize(
     url: Env.supabaseUrl,
     anonKey: Env.supabaseAnonKey,
-    debug: true,
+    debug: Env.environment == 'dev',
   );
 
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Meu App de Testes',
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.teal),
-      home: TmtA(),
-  }
 }
