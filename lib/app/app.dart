978dbc84c7fd895427c72aaa264c2fdc251ed686
Lyandra Lisboa <lyandralisboa08@gmail.com>
2025-10-13 20:12:28 -0300

    feat: atualiza manter sessão ativa

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final appRouter = createAppRouter(context);
     return MaterialApp.router(
       title: AppConstants.appName,
       theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.teal),
